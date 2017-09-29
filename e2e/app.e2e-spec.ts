a2eb7a28081fc1e8342ec4cdf1a0edbd46f4f53b
Jheison L Rodriguez <jrodr12@bancodebogota.com.co>
2017-09-29 15:57:14 -0500

    Adding circle ci config5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('proplistings App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect<any>(page.getParagraphText()).toEqual('app works!');
+    expect<any>(page.getParagraphText()).toEqual('Welcome to app');
   });
 });
